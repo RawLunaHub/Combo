0c73230c4afa8b3b7ada81d32d0dd6383f2b57b8
RawLunaHub <83168446+RawLunaHub@users.noreply.github.com>
2022-12-03 19:42:29 +0300

    Update ULua.lua

--- ULua.lua
+++ ULua.lua
@@ -1,9 +1,9 @@
-G2L = v2
 
-local v2 = {};
+
+local G2L = {};
 
 -- StarterGui.Combo
-G2L["1"] = Instance.new("ScreenGui", game:GetService("CoreGui")
+G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
 G2L["1"]["Name"] = [[Combo]];
 G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
 
@@ -16,9 +16,9 @@ G2L["2"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
 G2L["2"]["Size"] = UDim2.new(0, 200, 0, 50);
 G2L["2"]["Text"] = ComboText;
 G2L["2"]["Name"] = [[CMB]];
-v2["2"]["Font"] = Enum.Font.FredokaOne;
-v2["2"]["BackgroundTransparency"] = 1;
-v2["2"]["Position"] = UDim2.new(0.43853211402893066, 0, 0.8246913552284241, 0);
+G2L["2"]["Font"] = Enum.Font.FredokaOne;
+G2L["2"]["BackgroundTransparency"] = 1;
+G2L["2"]["Position"] = UDim2.new(0.43853211402893066, 0, 0.8246913552284241, 0);
 
 
-return v2["1"], require;
+return G2L["1"], require;
